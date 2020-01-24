f04bb7ae90fcb7d9f0585ca1f210e3ae3f3d37a4
Paulo Ricardo Carvalho Horacio <phoracio@gruporecovery.com>
2020-01-24 11:43:00 -0300

    Parte II Aula 3

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,6 +4,18 @@ class  NegociacaoController{
 
     adiciona(event){
         event.preventDefault();
+
+        let $ = document.querySelector.bind(document);
+
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
+
         alert('Chamei ação no controller');
     }
 }
+
