4c2a614926de23b1a346b444b36aaf73cc8d53e2
Paulo Ricardo <pauloricardoh@hotmail.com>
2020-03-04 23:44:22 -0300

    Métodos Estaticos Aula 4

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -14,35 +14,14 @@ class  NegociacaoController{
   
     adiciona(event){   
         event.preventDefault();
-<<<<<<< HEAD
-          
-       
-        let helper = new DateHelper();
-    
-        let negociacao = new Negociacao (
-            helper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
-                                    );
-         console.log(negociacao); 
-         console.log(helper.dataParaTexto(negociacao.data));
-=======
-           
-        let helper = new DateHelper(); 
-       
+            
         let negociacao = new Negociacao (
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
-         );
-
-      // console.log(negociacao);
-      // console.log(DateHelper.dataParaTexto(negociacao.data));
-
-      this._listaNegociacoes.adiciona(negociacao);
-      console.log(this._listaNegociacoes.negociacoes);
-
->>>>>>> 8901cccb115b936799eda4c165d4b972eae77d3a
+                                    );
+         console.log(negociacao); 
+         console.log(DateHelper.dataParaTexto(negociacao.data));
     }
 }
 
--- javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,31 +1,19 @@
 
 class DateHelper{
 
-<<<<<<< HEAD
-    dataParaTexto(data){
+    constructor(){
+        throw new Error('Esta classe não pode ser instanciada');
+
+    }
+
+   static dataParaTexto(data){
         return data.getDate()
              + '/' + (data.getMonth() + 1)
              + '/' + data.getFullYear();
                       }
   
-    textoParaData(texto){
+    static textoParaData(texto){
         return new Date(...texto.split('-').map((item,indice) =>  item - indice % 2 ));       
     }
   
 }
-=======
-    /*constructor(){
-        throw new Error('DateHelper não pode ser instanciada');
-        }*/
-
-   static dataParaTexto(data){
-        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()} `;
-    }
-
-    static textoParaData(texto){
-      if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
-            throw new Error('Deve estar no formato aaaa-mm-dd');
-        return new Date(...texto.split('-').map((item,indice) => item - indice % 2));
-    }
-}
->>>>>>> 8901cccb115b936799eda4c165d4b972eae77d3a
