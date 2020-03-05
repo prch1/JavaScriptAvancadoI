92f5bf51e655d06a8685b40f3d25e66c607b1c2e
Paulo Ricardo <pauloricardoh@hotmail.com>
2020-03-05 00:44:20 -0300

    ListModel Aula 4

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -9,18 +9,33 @@ class  NegociacaoController{
             this._inputData = $('#data');
             this._inputQuantidade = $('#quantidade');
             this._inputValor = $('#valor');
+            this._listaNegociacoes = new ListaNegociacoes();
         }
   
     adiciona(event){   
         event.preventDefault();
             
-        let negociacao = new Negociacao (
-            DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
-                                    );
-         console.log(negociacao); 
-         console.log(DateHelper.dataParaTexto(negociacao.data));
+        this._listaNegociacoes.adiciona(this._criarNegociacao());
+        this._limpaFormulario(); 
+        console.log(this._listaNegociacoes.negociacoes);                             
     }
+
+     _limpaFormulario(){
+         this._inputData.value='';
+         this._inputQuantidade.value =1;
+         this._inputValor.value = 0.0;
+
+         this._inputData.focus();
+     }
+
+     _criarNegociacao(){
+         return new Negociacao(
+             DateHelper.textoParaData(this._inputData.value),
+             this._inputQuantidade.value,
+             this._inputValor.value
+         );
+     }
+
+
 }
 
