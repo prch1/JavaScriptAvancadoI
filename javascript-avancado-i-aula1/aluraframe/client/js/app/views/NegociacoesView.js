bb00553f82080b83e88f1784c0380680d7235384
Paulo Ricardo <pauloricardoh@hotmail.com>
2020-03-25 22:33:42 -0300

    Aula 5 Montar tabela

--- javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,17 +1,31 @@
-<table class="table table-hover table-bordered">
-<thead>
-    <tr>
-        <th>DATA</th>
-        <th>QUANTIDADE</th>
-        <th>VALOR</th>
-        <th>VOLUME</th>
-        <th>Calculo</th>
-    </tr>
-</thead>
 
-<tbody>
-</tbody>
 
-<tfoot>
-</tfoot>
-</table>
+class NegociacoesView{
+
+  constructor(elemento){
+      this._elemento = elemento;
+  }
+
+ _template(){
+
+    return `
+    <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                        <th>VOLUME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+         </table>
+        `;
+    
+    }
+
+    update(){
+        this._elemento.innerHTML = this._template();
+    }
+}
